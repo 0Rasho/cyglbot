0ba748cb619c1500d8cfb833837877e5a441c4d0
Rasho <Rasho@no-email.com>
2022-04-30 13:51:54 +0530

    add hrefs to news

--- lib/motd.py
+++ lib/motd.py
@@ -1,6 +1,18 @@
 import feedparser
 import random
 
+class Feed():
+    def __init__(self, title, link):
+        self.title = title
+        self.link = link
+
+    def link(self):
+        return self.link
+
+    def title(self):
+        return self.title
+
+
 class RSSfeeds():
     def __init__(self, url):
         self.url = url
@@ -10,12 +22,14 @@ class RSSfeeds():
         feeds = feedparser.parse(self.url)
         for entry in feeds.entries:
             if entry.title not in self.news:
-                self.news.append(entry.title)
+                self.news.append(Feed(entry.title, entry.link))
 
     def shuffle(self):
         random.shuffle(self.news)
 
     def clear_feeds():
+        for i in self.news:
+            del i
         self.news = []
 
 class RSS():
@@ -32,12 +46,17 @@ class RSS():
             "https://www.aljazeera.com/xml/rss/all.xml",
             "https://www.newsrust.com/feeds/posts/default?alt=rss",
             "https://feeds.thelocal.com/rss/es",
-            "https://wan-ifra.org/news/feed/",
             "https://247newsaroundtheworld.com/feed/",
             "https://www.dailytelegraph.com.au/news/world/rss",
             "https://www.channelnewsasia.com/api/v1/rss-outbound-feed?_format=xml&category=6311"
             "https://feeds.a.dj.com/rss/RSSWorldNews.xml",
-            "https://www.worldpress.org/feeds/topstories.xml"
+            "https://www.worldpress.org/feeds/topstories.xml",
+            "https://www.ft.com/global-economy?format=rss",
+            "https://www.sciencedaily.com/rss/all.xml",
+            "https://moxie.foxnews.com/feedburner/world.xml",
+            "https://globalnews.ca/world/feed/",
+            "https://www.thehindu.com/news/international/feeder/default.rss",
+            "https://allafrica.com/tools/headlines/rdf/latest/headlines.rdf"
             #"https://feeds.24.com/articles/news24/World/rss"
         ]
         self.rssobjs=[]
@@ -60,18 +79,14 @@ class RSS():
             j = 0
             random.shuffle(rss.news)
             for i in rss.news:
-                il = i.lower()
-                if "how" in il: 
+                il = i.title.lower()
+                if "latest videos" in il:
                     continue
-                elif "why" in il:
-                    continue
-                elif "explained" in il:
-                    continue
-                elif "latest videos" in il:
+                if "latest news" in il:
                     continue
                 self.news.append(i)
                 j = j + 1
-                if j == 2:
+                if j == 1:
                     break
             random.shuffle(self.news)
 
@@ -91,6 +106,7 @@ class MOTD():
         self.robj.rss_populate()
         motd = self.motd_start + self.font
         for i in self.robj.news:
-            motd += self.breakgif + i
+            motd += self.breakgif + "<a target=\"_blank\" rel=\"noopener noreferrer\" href=\""
+            motd +=i.link+"\">"+i.title+"</a>"
         motd += self.fontend + self.motd_end
         return motd
